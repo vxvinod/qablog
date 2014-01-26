e1f9b15e482726a1d151b3429a6e0d04a8a4bda0
vxvinod <vinodkmrmurugan@gmail.com>
2014-01-26 23:11:18 +0530

    snappy app first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Qablog::Application.config.secret_token = '565235fc25b7e65a7a3fdf295728e8622f6bf9574e86fdf6f63546e6aaaea8f2d99bc0cb92a82b49621937eeae2f6a82413a057f4f8d70f559bec63c873966c6'
